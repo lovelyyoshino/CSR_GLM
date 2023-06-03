59414f2ac0b53f1fea35f3c2f5747ef8aa00eb5b
lovelyyoshino <mpl9725440@gmail.com>
2023-06-03 11:45:26 +0800

    int4 local

--- chatglm/examples/quantized_finetune_with_local_model.sh
+++ chatglm/examples/quantized_finetune_with_local_model.sh
@@ -8,8 +8,8 @@ CUDA_VISIBLE_DEVICES=0 python ../src/train_sft.py \
     --finetuning_type p_tuning \
     --output_dir path_to_sft_checkpoint \
     --overwrite_cache \
-    --per_device_train_batch_size 4 \
-    --gradient_accumulation_steps 4 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 1 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
     --save_steps 1000 \
